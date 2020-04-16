2f28be3b05eddbaec85c25e493b40849625747f4
fitzgerald1337 <fitzgerald1337@gmail.com>
2020-04-15 23:24:24 -0500

    controller change

--- controllers/car.controllers.js
+++ controllers/car.controllers.js
@@ -1,41 +1,45 @@
 const Car = require('../models/cars.models');
-const Mongoose=require('mongoose')
+const Mongoose = require('mongoose')
 // const User = require('../models/users.models')
 
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
 
-exports.addCar = async (req, res, next) => {
-    // console.log(req.body)
-    const userById = await User.findById(id)
-    await userById.save();
-    return res.send(userById);
+exports.addCar = (req, res, next) => {
+    //     console.log(req.body)
+    //     const userById = await User.findById(id)
+    //     await userById.save();
+    //     return res.send(userById);
+
+    Car.create(req.body)
+        .then(response => res.json(response))
+        .catch(err => console.log(err))
 };
 
 exports.getCars = (req, res, next) => {
-     // Car.findById(req.params.Cid)
+    // Car.findById(req.params.Cid)
     Car.find()
         .then(response => res.json(response))
         .catch(err => console.log(err));
 };
 exports.getCar = (req, res, next) => {
     // console.log(req.params.Cid);
-    const {Cid} = req.params;
+    const { Cid } = req.params;
     Car.findById(Cid)
-       .then(response => res.json(response))
-       .catch(err => console.log(err));
+        .then(response => res.json(response))
+        .catch(err => console.log(err));
 };
 
 exports.getCarByUserId = (req, res, next) => {
-  
-  const uid=Mongoose.Types.ObjectId(req.params.userId)
-//   console.log(uid,req.params.userId)
-    Car.find({'comments.writer' : uid})
-    .then(cars => {
-        res.json(cars)
-        
-    })
+
+    const uid = Mongoose.Types.ObjectId(req.params.userId)
+    //   console.log(uid,req.params.userId)
+    Car.find({ 'comments.writer': uid })
+        .then(cars => {
+            res.json(cars)
+
+        })
 }
 
 
@@ -54,28 +58,28 @@ exports.postSignup = async (req, res, next) => {
     });
 };
 
-exports.postLogin =async (req, res, next) => {
-        passport.authenticate('login', async (err, user, info) => {
-            try {
-                if (err || !user) {
-                    const error = new Error('An Error occurred')
-                    return next(error);
-                }
-                req.login(user, { session: false }, async (error) => {
-                    if (error) return next(error)
-                    //We don't want to store the sensitive information such as the
-                    //user password in the token so we pick only the email and id
-                    const body = { _id: user._id, email: user.email };
-                    //Sign the JWT token and populate the payload with the user email and id
-                    const token = jwt.sign({ user: body }, 'top_secret');
-                    //Send back the token to the user
-                    return res.json({ token });
-                });
-            } catch (error) {
+exports.postLogin = async (req, res, next) => {
+    passport.authenticate('login', async (err, user, info) => {
+        try {
+            if (err || !user) {
+                const error = new Error('An Error occurred')
                 return next(error);
             }
-        })(req, res, next);
-    };
+            req.login(user, { session: false }, async (error) => {
+                if (error) return next(error)
+                //We don't want to store the sensitive information such as the
+                //user password in the token so we pick only the email and id
+                const body = { _id: user._id, email: user.email };
+                //Sign the JWT token and populate the payload with the user email and id
+                const token = jwt.sign({ user: body }, 'top_secret');
+                //Send back the token to the user
+                return res.json({ token });
+            });
+        } catch (error) {
+            return next(error);
+        }
+    })(req, res, next);
+};
 
 exports.getProfile = (req, res, next) => {
     //We'll just send back the user details and the token
